9aca5af0ac5fbd85efc699d8fdf626fd46de7358
hermaDC <diazcabezas.m1@gmail.com>
2024-09-12 17:23:09 +0200

    Mejorada retroalimentacion

--- app.py
+++ app.py
@@ -31,22 +31,22 @@ def index():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+        data = request.get_json()  # Obtener datos en formato JSON
+        username = data.get('username')
+        password = data.get('password')
         hashed_password = generate_password_hash(password, method='sha256')
         
         # Verificar si el usuario ya existe
         user_exists = User.query.filter_by(username=username).first()
         if user_exists:
             flash('El nombre de usuario ya está en uso.')
-            return redirect(url_for('register'))
+            return jsonify({'succes': False, 'message': 'El usuario ya existe', 'redirect': url_for('register')}), 401
         
         # Crear nuevo usuario
         new_user = User(username=username, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
-        flash('Registro exitoso. ¡Inicia sesión ahora!')
-        return redirect(url_for('login'))
+        return jsonify({'succes': True, 'message': 'Usuario registrado, inicia sesión', 'redirect': url_for('login')}), 200
     
     return render_template('register.html')
 
@@ -54,8 +54,9 @@ def register():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+        data = request.get_json()  # Obtener datos en formato JSON
+        username = data.get('username')
+        password = data.get('password')
         
         # Buscar el usuario en la base de datos
         user = User.query.filter_by(username=username).first()
@@ -64,9 +65,9 @@ def login():
         if user and check_password_hash(user.password, password):
             session['user_id'] = user.id
             flash('Inicio de sesión exitoso.')
-            return redirect(url_for('dashboard'))
+            return jsonify({'success': True, 'message': 'Login exitoso', 'redirect': url_for('dashboard')}), 200
         else:
-            flash('Usuario o contraseña incorrectos.')
+            return jsonify({'success': False, 'message': 'Usuario o contraseña incorrectos'}), 401
     
     return render_template('login.html')
 
